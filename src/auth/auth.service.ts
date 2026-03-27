f91690b6d32c861c70fba24f744699fca85340c0
Yeamin Foysal <yeaminfoysal14@gmail.com>
2026-03-28 01:39:07 +0600

    user register with jwt token complete

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dto/Register.dto';
+import bcrypt from "bcrypt";
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) { }
+    constructor(
+        private readonly userService: UserService,
+        private jwtService: JwtService
+    ) { }
 
-    register(registerDto: RegisterDto) {
+    async register(registerDto: RegisterDto) {
 
-        const user = this.userService.getEmailById(registerDto.email);
+        const user = await this.userService.getEmailById(registerDto.email);
+        if (user) {
+            throw new ConflictException("Email already exists;")
+        }
 
-        return user;
+        const saltRound = 10;
+        const hashedPass = await bcrypt.hash(registerDto.password, saltRound)
+
+        const newUser = await this.userService.registerUser({
+            ...registerDto,
+            password: hashedPass,
+        })
+
+        const payload = { id: newUser.id, email: newUser.email };
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        };
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDto } from 'src/auth/dto/Register.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UserService {
-    getEmailById(email: string) {
-        return {
-            email,
-            message: "user service source"
-        }
+    constructor(private PrismaService: PrismaService) { }
+
+    async getEmailById(email: string) {
+        const user = await this.PrismaService.user.findFirst(
+            { where: { email: email } }
+        )
+        return user;
+    }
+
+    async registerUser(registerDto: RegisterDto) {
+        const user = await this.PrismaService.user.create(
+            { data: registerDto }
+        );
+        return user
     }
 }
